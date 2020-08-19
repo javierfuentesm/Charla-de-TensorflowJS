70282ed42e62b926f35fa953b014b92980ba1d07
Javier <javier.fuentesm@hotmail.com>
2020-08-18 20:23:00 -0400

    Se termina el primer ejemplo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ml5 from "ml5";
+import leon from "./leon.jpg";
 
 function App() {
+  const [predictions, setPredictions] = useState([]);
+  const imgRef = useRef();
+
+  const classify = async () => {
+    const classifier = ml5.imageClassifier("MobileNet", () => {
+      console.log("El modelo esta listo ");
+    });
+
+    const result = await classifier.predict(
+      imgRef.current,
+      5,
+      (err, results) => results
+    );
+    setPredictions(result);
+  };
+
+  useEffect(() => {
+    classify();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={leon} ref={imgRef} id="image" width="400" alt="" />
+        {predictions.length > 0 && (
+          <>
+            {predictions.map(({ label, condifence }, index) => (
+              <div key={index + ""}>
+                {index + 1}. Prediction: {label} at {condifence}{" "}
+              </div>
+            ))}
+          </>
+        )}
       </header>
     </div>
   );
